321e5148dfcac44c43ad385be08c034745c4ebc6
AuroraNasa <b04031010@outlook.com>
2025-07-04 21:22:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
           #SETTINGS
           CURRENT_TIME_VERSIONCODE=$(date +'%Y%m%d')
-          Github_update_repo=Aurora-Nasa-1/AMMF
-          MODID=TEST
-          MODNAME=TEST
+          Github_update_repo=Aurora-Nasa-1/K50SoundBoostPlus
+          MODID=K50SoundBoostPlus
+          MODNAME=k50音质音效增强
